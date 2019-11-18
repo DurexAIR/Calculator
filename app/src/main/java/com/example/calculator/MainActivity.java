9850f547db907509b0502990163b8c245df2aa44
DurexAIR <49096399+DurexAIR@users.noreply.github.com>
2019-11-18 20:13:14 +0800

    1.增加登录、注册、首页

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,13 +3,54 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button bt1;
+    private Button bt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        bt1=(Button)findViewById(R.id.btn_login);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jumpToLayout1();
+            }
+        });
+        bt2=(Button)findViewById(R.id.btn_signin);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jumpToLayout2();
+            }
+        });
     }
+    public void jumpToLayout1(){
+        setContentView(R.layout.activity_login);
+//        bt2=(Button)findViewById(R.id.btn_signin);
+//        bt2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                jumpToLayout1();
+//            }
+//        });
+    }
+    public void jumpToLayout2(){
+        setContentView(R.layout.activity_sign);
+//        bt1=(Button)findViewById(R.id.button1);
+//        bt1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                jumpToLayout2();
+//            }
+//        });
+    }
+
 }
